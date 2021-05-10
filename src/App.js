e978ab0dcf2b306f0e8b852d7361ded3861f6ce5
andypin2002 <andypin2002@gmail.com>
2021-05-10 17:55:29 +0200

    10/05/21-17:55

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <Head1/>
+      <Head2/>
+      <h2>Informacion Personal</h2>
+      <div id="InformacionPersonal">
+        <PerInfo x="N° de documento:" y="text"/>
+        <PerInfo x="Nombre:" y="text"/>
+        <PerInfo x="Apellidos:" y="text"/>
+        <PerInfo x="Email:" y="text"/>
+        <PerInfo x="Telefono:" y="text"/>
+        <PerInfo x="Fecha de nacimiento:" y="date"/>
+        <label for="gender">Género:</label>
+        <select id="gender">
+          <option value="Masculino">Masculino</option>
+          <option value="Femenino">Femenino</option>
+        </select>
+      </div>
+      <h2>Contacto</h2>
+      <div id="Contacto">
+        <PerInfo x="Direccion:" y="text"/>
+        <PerInfo x="Ciudad:" y="text"/>
+        <PerInfo x="Codigo postal:" y="text"/>
+        <PerInfo x="Direccion" y="text"/>
+        <label for="pais">País</label>
+        <select id="Pais:">
+          <option value="España">España</option>
+          <option value="Italia">Italia</option>
+          <option value="EEUU">EEUU</option>
+        </select>
+      </div>
+      <h2>Informacion Adicional</h2>
+      <div id="InformacionAdicional">
+        <PerInfo x="Estudios:" y="text"/>
+        <PerInfo x="Ocupacion:" y="text"/>
+        <PerInfo x="Aficiones:" y="text"/>
+      </div>
+      <button class="aceptarButton" onClick="">ACEPTAR</button>
+    </div>
+    
+    
+  );
+}
+const Head1 = () =>{
+  return(
+    <div class="headRow">
+      <div class="headItem1"><h1 className="App-header1">Agregar un nuevo paciente</h1></div>
+      
+      <div class="headItem2"><button className="volverButton">VOLVER</button></div>
+    </div>
+  );
+}
+
+const Head2 = () =>{
+  return(
+    <div>
+      <h2 className="App-header2" >DATOS DEL PACIENTE </h2>
+    </div>
+    
+  );
+}
+const PerInfo = (props) =>{
+  return(
+    <div class="campo">
+      <div>{ props.x }</div>
+        <input name={ props.x } type={ props.y } ></input>
     </div>
   );
 }
